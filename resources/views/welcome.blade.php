6bfb0a72edb4649f50273da003f9bf617dc1e516
Karlis2552 <karlatherina@gmail.com>
2019-05-04 12:20:16 -0400

    archivos iniciales

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class= "jumbotron text-center">
+    <h1>Laratter</h1>
+    <nav>
+         <ul class="nav nav-pills">
+            <li class="nav-item">
+                <a class="nav-link" href="/">Home</a>
+            </li>
+            
+        </ul>
+    </nav>
+</div>
+<div class="row">
+    <form action="/messages/create" method="post">
+        <div class= "form-group">
+                {{csrf_field()}}
+                <input type="text" name="message" class="form-control" 
+                placeholder="que estas pensando?">
+        </div>
+    </form>
+</div>
+<div class="row">
+    @forelse($messages as $message)
+        <div class="col-6">
+            <img class="img-thumbnail" src="{{$message->image }}">
+            <p class="card-text">
+                {{ $message->content}}
+                <a href="/messages/{{ $message->id }}" >Leer más</a>
+            </p>
+        </div>
+        @empty
+            <p> No hay mensajes destacados </p>
+    @endforelse
+</div>
+@endsection
